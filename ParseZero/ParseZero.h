3fda91e404180792b480b6b0e2b838faa6320e0d
Florent Vilmart <florentvilmart@me.com>
2015-11-23 12:17:59 -0500

    Initial Commit

---
+++ ParseZero/ParseZero.h
@@ -0,0 +1,19 @@
+//
+//  ParseZero.h
+//  ParseZero
+//
+//  Created by Florent Vilmart on 15-11-23.
+//  Copyright © 2015 flovilmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParseZero.
+FOUNDATION_EXPORT double ParseZeroVersionNumber;
+
+//! Project version string for ParseZero.
+FOUNDATION_EXPORT const unsigned char ParseZeroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseZero/PublicHeader.h>
+
+
