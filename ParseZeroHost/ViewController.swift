9296f0e9bb17d8d1e0091180ec0de4a7b070ae38
Florent Vilmart <florentvilmart@me.com>
2015-11-24 09:03:20 -0500

    Adds test app - Moves parse - Adds linked flag to play nice with Parse and Bolts as non dylibs while integrating in App

---
+++ ParseZeroHost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseZeroHost
+//
+//  Created by Florent Vilmart on 15-11-24.
+//  Copyright © 2015 flovilmart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
